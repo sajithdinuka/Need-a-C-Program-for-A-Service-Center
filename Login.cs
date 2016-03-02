105e1e9844768cc4585e3146cd995d3be9ea2ccb
sajithdinuka <sajithdinukavikasitha@ymail.com>
2016-03-03 02:21:04 +0530

    Added files via upload

---
+++ Login.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using SevenStarServiceCenter.Database;
+
+
+namespace SevenStarServiceCenter
+{
+    public partial class frmLogin : Form
+    {    
+        DataSet DS = new DataSet();
+        SqlDataAdapter sqlDA=new SqlDataAdapter();       
+        dbaseClass conObj = new dbaseClass();
+        int rowCount;
+         SevenStarServiceCenterSSSC SSSC = new SevenStarServiceCenterSSSC();
+        //<<<<<<<<<<<<< VARIABLE FOR FILE HANDALING  >>>>>>>>>>>>>>>>>>>>>>
+
+        public frmLogin()
+        {
+            InitializeComponent();
+
+        }       
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+
+            //<<<<<<<<<<<<< Key HANDALING  >>>>>>>>>>>>>>>>>>>>>>
+            if (keyData == Keys.Escape)/*<<If the user has enterd |Esc| key, following message will display>>*/
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you really want to terminate the system?", "SSSC Management - Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                /*<<Message>>*/
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Exit();
+                    return true;
+                }
+                else
+                {
+                    lblError.Text = "Not Terminated";
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conObj.sqlconn.State == ConnectionState.Closed)
+                {
+                    conObj.sqlconn.Open();//opening database connection
+                }
+                string sqllogin = "SELECT * FROM login WHERE user_name='" + txtUName.Text.Trim() + "' AND password='" + txtUPass.Text.Trim() + "'";//sql coding
+
+                sqlDA = new SqlDataAdapter(sqllogin, conObj.sqlconn);//setting sql statment to the data adaptor
+                
+                sqlDA.Fill(DS, "log");
+
+                if (DS.Tables["log"].Rows.Count > 0)
+                {
+                    dbaseClass.loginName = txtUName.Text;
+
+                    this.Close();//hide current form
+                    SSSC.Show();//showing main frame
+                    SSSC.Enabled = true;//enabling main frame
+                    SSSC.Focus();
+                }
+                else
+                {
+                    //message if the username or password incorrect
+                    lblError.Text = "Invalid username or password";
+                    txtUName.Text = "";
+                    txtUPass.Text = "";
+                    txtUName.Focus();
+                }
+            }
+            catch (NullReferenceException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            try
+       
+            {         SSSC.Show();
+                SSSC.Enabled = false;
+
+                conObj.databaseconnect();
+                if (conObj.sqlconn.State == ConnectionState.Closed)
+                {
+                    conObj.sqlconn.Open();
+                }
+                string sqlText = "SELECT * FROM login";
+                conObj.sqlDA = new SqlDataAdapter(sqlText, conObj.sqlconn);
+
+                conObj.sqlDA.Fill(DS, "stDET");
+
+                rowCount = DS.Tables["stDET"].Rows.Count - 1;
+
+                //closing the connection
+                conObj.sqlconn.Close();
+                lblError.Text = "";
+                btnLogin.Enabled = false;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Database Not Connected");
+                Application.Exit();
+            }
+        }
+
+        private void txtUName_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtUName.Text))
+            {
+                btnLogin.Enabled = false;
+            }
+            else
+            {
+                btnLogin.Enabled = true;
+            }
+        }
+
+        private void txtUPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUName_Leave_1(object sender, EventArgs e)
+        {
+            string val;
+            val = txtUName.Text;
+            if (val.Length == 0)
+            {
+                lblError.Text="User name Can not be a blank";
+                txtUName.Focus();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+            lblDate.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e, string value)
+        {
+            lblError.Text = "";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Login.designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SevenStarServiceCenter
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUPass = new System.Windows.Forms.TextBox();
+            this.txtUName = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // txtUPass
+            // 
+            this.txtUPass.Location = new System.Drawing.Point(130, 317);
+            this.txtUPass.Name = "txtUPass";
+            this.txtUPass.PasswordChar = '*';
+            this.txtUPass.Size = new System.Drawing.Size(126, 20);
+            this.txtUPass.TabIndex = 2;
+            this.txtUPass.TextChanged += new System.EventHandler(this.txtUPass_TextChanged);
+            // 
+            // txtUName
+            // 
+            this.txtUName.Location = new System.Drawing.Point(130, 280);
+            this.txtUName.Name = "txtUName";
+            this.txtUName.Size = new System.Drawing.Size(126, 20);
+            this.txtUName.TabIndex = 1;
+            this.txtUName.TextChanged += new System.EventHandler(this.txtUName_TextChanged);
+            this.txtUName.Leave += new System.EventHandler(this.txtUName_Leave_1);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(230, 358);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(12, 356);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(35, 13);
+            this.lblError.TabIndex = 3;
+            this.lblError.Text = "label3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(162, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(156, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Seven Star ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(133, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Service Center";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(18, 404);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 13);
+            this.lblTime.TabIndex = 9;
+            this.lblTime.Text = "lblTime";
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(18, 385);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(40, 13);
+            this.lblDate.TabIndex = 47;
+            this.lblDate.Text = "lblDate";
+            this.lblDate.Click += new System.EventHandler(this.lblDate_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 434);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtUName);
+            this.Controls.Add(this.txtUPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUPass;
+        private System.Windows.Forms.TextBox txtUName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
